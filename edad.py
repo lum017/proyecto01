3ee76fca20c8ce1628af988a4e6332e4cf4e5a9b
lauraUsuga <laurausugam.05gmail.com>
2024-01-18 07:35:00 -0500

    esto fue modificado

--- edad.py
+++ edad.py
@@ -1,4 +1,4 @@
-nombre = input ( "ingrese por favor su nombre: ")
+nombre = input ( "ingrese por favor su nombre porfavor porfavor: ")
 edad=int (input("Ingrese su edad: "))
 if edad>=18:
     print(f"{nombre},eres mayor de edad")
